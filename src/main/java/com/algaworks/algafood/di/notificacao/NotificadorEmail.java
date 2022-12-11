327e53262890933b2747318f14abea7defac800e
guilherme <guigarks@gmail.com>
2022-12-10 22:43:42 -0300

    Aula 2.14 Criando pontos de injeção com @Autowired

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -4,25 +4,13 @@ import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
+@Component
 public class NotificadorEmail implements Notificador{
-	
-	public boolean caixaAlta;
-	public String hostServidorSmtp;
-	
-	public NotificadorEmail(String hostServidorSmtp) {
-		this.hostServidorSmtp = hostServidorSmtp;
-		System.out.println("NotificadorEmail");
-	}
-	
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		if(this.caixaAlta) {
-			mensagem =  mensagem.toUpperCase();
-		}
-		
-		System.out.printf("Notificando %s atraves do e-mail %s usando SMTP %s: %s\n", 
-				cliente.getNome(),cliente.getEmail(), this.hostServidorSmtp, mensagem);
+		System.out.printf("Notificando %s atraves do e-mail %s %s\n", 
+				cliente.getNome(),cliente.getEmail(), mensagem);
 		
 	}
 
@@ -32,12 +20,5 @@ public class NotificadorEmail implements Notificador{
 		// TODO Auto-generated method stub
 		
 	}
-
-
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
-	}
-	
-	
 
 }
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,22 +1,31 @@
 package com.algaworks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 import com.algaworks.algafood.di.notificacao.NotificadorEmail;
 
-
+@Component
 public class AtivacaoClienteService {
 	
+	@Autowired//consegue injetar mesmo sendo um atributo private.
 	private Notificador notificador;
 	
+	@Autowired//
 	public AtivacaoClienteService(Notificador notificador) { //construtor com parametro injetado
 		this.notificador = notificador;
 		
-		System.out.println("AtivacaoClienteService" + notificador);
 	}
 	
+	/*
+	 * A @Autowired pode ser usada para definir
+	 * varios pontos de injenção, no construtor,
+	 * no metodo setter, e tambem no atributo.
+	 * pode ser usado em qualquer um deles.
+	 * 
+	 * */
 
 	public void ativar(Cliente cliente) {
 		/*O metodo de notifica��o e chamado
@@ -28,4 +37,6 @@ public class AtivacaoClienteService {
 		
 	}
 
+	
+	
 }
